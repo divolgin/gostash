c5976a630e7f774d72e0ba34bf7c13eb8357f439
Dmitriy Ivolgin <divolgin@liveperson.com>
2014-04-04 18:12:40 -0400

    Turned this into loggo plugin

---
+++ formatter.go
@@ -0,0 +1,66 @@
+package gostash
+
+import (
+	"encoding/json"
+	"github.com/lookio/loggo"
+	"path/filepath"
+	"strconv"
+	"time"
+)
+
+type Formatter struct {
+	message  logstashMessage
+	metadata map[string]string
+}
+
+type logstashMessage struct {
+	Timestamp string            `json:"@timestamp"`
+	Message   string            `json:"message"`
+	Type      string            `json:"type"`
+	Fields    map[string]string `json:"fields"`
+}
+
+func NewFormatter(config *Config) *Formatter {
+	formatter := Formatter{}
+
+	formatter.metadata = make(map[string]string)
+	if config.ThisHostName != "" {
+		formatter.metadata["host"] = config.ThisHostName
+	}
+	if config.CodeVersion != "" {
+		formatter.metadata["version"] = config.CodeVersion
+	}
+
+	if config.InputType == "" {
+		formatter.message.Type = "go"
+	} else {
+		formatter.message.Type = config.InputType
+	}
+
+	return &formatter
+}
+
+func (formatter *Formatter) Format(level loggo.Level, module, filename string, line int, timestamp time.Time, message string) string {
+	formatter.metadata["level"] = level.String()
+	formatter.metadata["module"] = module
+	formatter.metadata["file"] = filepath.Base(filename)
+	formatter.metadata["line"] = strconv.Itoa(line)
+	formatter.metadata["log.time"] = timestamp.In(time.UTC).Format("2006-01-02 15:04:05")
+	encoded, err := formatter.encode(message, formatter.metadata)
+	if err != nil {
+		return ""
+	}
+	return string(encoded[:])
+}
+
+func (formatter *Formatter) encode(msg string, metadata map[string]string) ([]byte, error) {
+	formatter.message.Timestamp = getTimeStamp()
+	formatter.message.Message = msg
+	formatter.message.Fields = metadata
+	return json.Marshal(formatter.message)
+}
+
+func getTimeStamp() string {
+	now := time.Now().UTC()
+	return now.Format("2006-01-02T15:04:05.000-07:00")
+}
--- gostash.go
+++ gostash.go
@@ -1,16 +1,15 @@
 package gostash
 
 import (
-	"encoding/json"
 	"net"
-	"time"
 )
 
-type logstashMessage struct {
-	Timestamp string            `json:"@timestamp"`
-	Message   string            `json:"message"`
-	Type      string            `json:"type"`
-	Fields    map[string]string `json:"fields"`
+type Config struct {
+	LogstashHost string
+	LogstashPort string
+	InputType    string
+	ThisHostName string
+	CodeVersion  string
 }
 
 type LogstashClient struct {
@@ -19,38 +18,48 @@ type LogstashClient struct {
 	serverAddr   *net.UDPAddr
 	conn         *net.UDPConn
 	err          error
-	msg          logstashMessage
+
+	formatter *Formatter
 }
 
-func NewLogstashClient(host string, port string, inputType string) *LogstashClient {
+func NewLogstashClient(config *Config) *LogstashClient {
 	c := LogstashClient{}
-	c.logstashHost = host
-	c.logstashPort = port
-	c.serverAddr, c.err = net.ResolveUDPAddr("udp", host+":"+port)
+	c.logstashHost = config.LogstashHost
+	c.logstashPort = config.LogstashPort
+	c.serverAddr, c.err = net.ResolveUDPAddr("udp", c.logstashHost+":"+c.logstashPort)
 	if c.err != nil {
 		// This is an invalid connection with error info
 		return &c
 	}
 	c.conn, c.err = net.DialUDP("udp", nil, c.serverAddr)
 
-	c.msg.Type = inputType
+	c.formatter = NewFormatter(config)
 
 	return &c
 }
 
+func (c *LogstashClient) Formatter() *Formatter {
+	return c.formatter
+}
+
 func (c *LogstashClient) Error() error {
-  return c.err
+	return c.err
 }
 
 func (c *LogstashClient) SendMessage(msg string, metadata map[string]string) {
 	if c.err != nil {
 		return
 	}
-	encoded, err := c.encode(msg, metadata)
+	encoded, err := c.formatter.encode(msg, metadata)
 	if err != nil {
 		return
 	}
-	go c.conn.Write(encoded)
+	c.Write(encoded)
+}
+
+func (c *LogstashClient) Write(msg []byte) (n int, err error) {
+	go c.conn.Write(msg)
+	return len(msg), nil
 }
 
 func (c *LogstashClient) Close() {
@@ -58,16 +67,4 @@ func (c *LogstashClient) Close() {
 		return
 	}
 	c.conn.Close()
-}
-
-func (c *LogstashClient) encode(msg string, metadata map[string]string) ([]byte, error) {
-	c.msg.Timestamp = getTimeStamp()
-	c.msg.Message = msg
-	c.msg.Fields = metadata
-	return json.Marshal(c.msg)
-}
-
-func getTimeStamp() string {
-	now := time.Now().UTC()
-	return now.Format("2006-01-02T15:04:05.000-07:00")
 }
